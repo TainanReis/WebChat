dbd10d054aae7165d96331ae0dc775146eab3a2c
TainanReis <tainanreis@yahoo.com>
2016-09-26 20:25:59 +0100

    Implemented a function that shows who connected and who left the chat. It also shows on the chat room who sent the message, not only on console.log anymore.

--- index.js
+++ index.js
@@ -23,20 +23,33 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/main.html');
 });
 //----- end of references
+var getUserId = function(socketId){ //to run the usersArray
+    for(var i in usersArray){
+            if (usersArray[i] == socketId){
+                return ++i; //Return user position @array AKA userId
+            }
+        }
+};
 
 //handling connection
 io.on('connection', function(socket){
     socket.on('message', function(msgObj){
-        for(var i in usersArray){
-            if (usersArray[i] == msgObj.userId){
-                var userId = ++i; //show userID as a number, not as socket
-                console.log('Message fromId ' + userId + ': ' + msgObj.message);
-            }
-        }
-        io.emit('message', msgObj.message);
+        var userId = getUserId(msgObj.userId);
+        console.log('ID ' + userId + ': ' + msgObj.message);
+        io.emit('message', userId + ': ' + msgObj.message);
     });
     socket.on('newConnection', function(data){ //when new connection
         usersArray.push(data); //add new user to users array
+        var userId = getUserId(data);
+        console.log(userId + ' has connected');
+        io.emit('message', userId + ' connected')
+    });
+    socket.on('disconnect', function(){ //when user disconnects
+        var socketId = socket.id;
+        var userId = getUserId(socketId.slice(2)); //when socket is disconnected _
+        //socket.io adds /# to the beggining of socket.id
+        console.log(userId + ' has Disconnected');
+        io.emit('message', userId + ' disconnected');
     });
 });
 
